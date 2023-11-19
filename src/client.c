40ad65aa3f38e86c8063bf07a22a8b09ceb3016f
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-11-19 18:29:50 -0500

    rev

--- src/client.c
+++ src/client.c
@@ -50,14 +50,10 @@ int client(char *host, int portnum, char uname[50]) {
   while(fgets(message,MAX_MESSAGE_SIZE,stdin) > 0){
     if (count == 0){
       strcpy(send_msg, uname);
-      strcat(send_msg, " has joined the server");
-      len = write(sockfd, send_msg, strlen(send_msg));
-      count++;
-    }else{
-      strcpy(send_msg, uname);
       strcat(send_msg, ":");
       strcat(send_msg, message);
       len = write(sockfd, send_msg, strlen(send_msg));
+      count++;
     }
     if (len < 0){
       printf("\n message not sent \n");
