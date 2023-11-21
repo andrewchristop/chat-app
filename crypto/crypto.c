76f38f6eb8e7ed08929aaf38d533bbcb16c1df1e
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-11-21 17:53:02 -0500

    test without aes encrypt

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -11,50 +11,13 @@ void processBlocks(char *msg, size_t blockSize, char cipher[1024]){
   for (size_t i = 0; i < sizeIB; i += blockSize){
     memset(block, 0, sizeof(block));
     strncpy(block, msg + i, blockSize);
-    AES_encrypt((const unsigned char *) block, temp, &aeskey);
+    //AES_encrypt((const unsigned char *) block, temp, &aeskey);
     
     if (i == 0){
-      strcpy(cipher, (char *)temp);
+      strcpy(cipher, block);
     }else{
-      strcat(cipher, (char *)temp);
+      strcat(cipher, block);
     }
 
   }
-  //strcpy(cipher, msg);
-  //unsigned char key[16] = "mysecretkey4444";
-  //size_t length = strlen(msg);
-  //AES_KEY aeskey;
-  //AES_set_encrypt_key(key, 128, &aeskey);
-  //
-  //for (size_t i = 0; i < length; i += blockSize){
-  //  char ciphertext[16];
-  //  size_t remainingLength = length - i;
-  //  size_t currentBlockSize;
-  //  
-  //  if (remainingLength < blockSize){
-  //    currentBlockSize = remainingLength;
-  //  }else{
-  //    currentBlockSize = blockSize;
-  //  }
-
-  //  char block[blockSize + 1];
-
-  //  strncpy(block, msg + i, currentBlockSize); 
-
-  //  for (size_t j = currentBlockSize; j < blockSize; ++j){
-  //    block[j] = '\0';
-  //  }
-
-  //  block[blockSize] = '\0';
-  //  AES_encrypt((const unsigned char*)block, (unsigned char *)ciphertext, &aeskey);
-
-  //  if (i == 0){
-  //    strcpy(cipher, ciphertext);
-  //  }else{
-  //    strcat(cipher, ciphertext);
-  //  }
-
-  //}
-
-
 }
