c30482508eb71b170d8a4dcdc7899b5cf3050645
Christopher Andrew <christopherandrew@pal-nat186-91-138.itap.purdue.edu>
2023-11-21 14:04:59 -0500

    crypto function coded

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -2,7 +2,26 @@
 
 
 void processBlocks(char *msg, size_t blockSize, char cipher[1024]){
-  strcpy(cipher, msg);
+  char block[16];
+  unsigned char temp[1024];
+  unsigned char key[16] = "mysecretkey1234";
+  size_t sizeIB = strlen(msg);
+  AES_KEY aeskey;
+  AES_set_encrypt_key(key, 128, &aeskey);
+  for (size_t i = 0; i < sizeIB; i += blockSize){
+    memset(block, 0, sizeof(block));
+    memset(temp, 0, sizeof(cipher));
+    strncpy(block, msg, blockSize);
+    AES_encrypt((const unsigned char *) block, temp, &aeskey);
+    
+    if (i == 0){
+      strcpy(cipher, (char *)temp);
+    }else{
+      strcat(cipher, (char *)temp);
+    }
+
+  }
+  //strcpy(cipher, msg);
   //unsigned char key[16] = "mysecretkey4444";
   //size_t length = strlen(msg);
   //AES_KEY aeskey;
