be65ea722998e5d716d6854d101b3354fe596666
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-11-21 19:27:04 -0500

    invoked memcpy()

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -8,16 +8,13 @@ void processBlocks(char *msg, size_t blockSize, char cipher[1024]){
   size_t sizeIB = strlen(msg);
   AES_KEY aeskey;
   AES_set_encrypt_key(key, 128, &aeskey);
+  size_t cipherIndex = 0;
+
   for (size_t i = 0; i < sizeIB; i += blockSize){
     memset(block, 0, sizeof(block));
     strncpy(block, msg + i, blockSize);
-    //AES_encrypt((const unsigned char *) block, temp, &aeskey);
-    
-    if (i == 0){
-      strcpy(cipher, block);
-    }else{
-      strcat(cipher, block);
-    }
-
+    AES_encrypt((const unsigned char *) block, temp, &aeskey);
+    memcpy(cipher + cipherIndex, temp, sizeof(temp));
+    cipherIndex += sizeof(temp);
   }
 }
