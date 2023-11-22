0bf471785a85f48f3f8b57998f7aa5b30be5d326
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-11-21 20:22:26 -0500

    changed key datatype to const

--- crypto/crypto.c
+++ crypto/crypto.c
@@ -4,7 +4,7 @@
 void processBlocks(char *msg, size_t blockSize, char cipher[1024]){
   unsigned char key[16] = "mysecretkey1234";
   AES_KEY aeskey;
-  AES_set_encrypt_key(key, 128, &aeskey);
+  AES_set_encrypt_key((const unsigned char *)key, 128, &aeskey);
   unsigned char iv[AES_BLOCK_SIZE];
   RAND_bytes(iv, AES_BLOCK_SIZE);
   AES_cbc_encrypt((const unsigned char *)msg, (unsigned char* )cipher, strlen(msg), &aeskey, iv, AES_ENCRYPT);
