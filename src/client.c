6e45fad3199f7a026dbf618f4d5a8f976c706f8f
Christopher Andrew <christopherandrew@Christophers-MacBook-Pro.local>
2023-12-02 10:27:31 -0500

    remove trailing newliens

--- src/client.c
+++ src/client.c
@@ -26,6 +26,7 @@ void *receiveMessage(void *socket) {
       unsigned char decrypted[MAX_MESSAGE_SIZE];
       decryptMessage(message, key, AES_BLOCK_SIZE, msgLen, bytesRead, decrypted);
       decrypted[bytesRead] = '\0';
+      decrypted[strcspn(decrypted, "\n")] = 0;
       //printf("%.*s", (int)msgLen, decrypted);
       fputs(decrypted, stdout);
       //fputs(message, stdout);
